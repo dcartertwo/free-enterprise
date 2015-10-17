6114392c50fac6c0886b5af2fcfe82ea7afc8b50
Dan <dl.carter00@gmail.com>
2015-10-17 15:48:32 -0400

    initial commit

---
+++ front/client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('frontApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
